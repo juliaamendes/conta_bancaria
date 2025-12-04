3fa034f3b0c942baacf62ab1d26b28c49bb8770b
Julia Mendes <cordeiroj647@gmail.com>
2025-12-04 11:10:00 -0300

    att 2

--- src/main/java/com/senai/contaBancaria/aplication/service/PagamentoAppService.java
+++ src/main/java/com/senai/contaBancaria/aplication/service/PagamentoAppService.java
@@ -1,20 +1,23 @@
 package com.senai.contaBancaria.aplication.service;
 
-import com.senai.contaBancaria.aplication.dto.ContaResumoDTO;
 import com.senai.contaBancaria.aplication.dto.PagamentoRegistroDto;
 import com.senai.contaBancaria.aplication.dto.PagamentoResponseDto;
 import com.senai.contaBancaria.domain.entity.Conta;
 import com.senai.contaBancaria.domain.entity.Pagamento;
 import com.senai.contaBancaria.domain.entity.Taxa;
 import com.senai.contaBancaria.domain.enums.FormaPagamento;
-import com.senai.contaBancaria.domain.repository.ContaRepository;
+import com.senai.contaBancaria.domain.exceptions.FormaDePagamentoInvalidaException;
+import com.senai.contaBancaria.domain.exceptions.SaldoInsuficienteException;
 import com.senai.contaBancaria.domain.repository.PagamentoRepository;
+import com.senai.contaBancaria.domain.repository.TaxaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -31,10 +34,10 @@ public class PagamentoAppService {
     public PagamentoResponseDto pagar(Long numero, PagamentoRegistroDto dto) {
         FormaPagamento formaPagamento = pagamentoDomainService.validarFormaPagamento(dto.formaPagamento());
 
-        BigDecimal taxas = (BigDecimal) taxaService.procurarTaxasPorFormaPagamento(formaPagamento);
+        Set<Taxa> taxas = taxaService.procurarTaxasPorFormaPagamento(formaPagamento);
         Conta conta = contaService.procurarContaAtiva(numero);
 
-        Pagamento pagamento = dto.toEntity(conta, (Set<Taxa>) taxas);
+        Pagamento pagamento = dto.toEntity(conta, taxas);
 
         BigDecimal valorTaxa = pagamentoDomainService.calcularTaxa(dto.valorPago(), taxas);
         BigDecimal valorTotal = pagamentoDomainService.validarSaldo(numero, dto.valorPago(), valorTaxa);
--- src/main/java/com/senai/contaBancaria/aplication/service/PagamentoDomainService.java
+++ src/main/java/com/senai/contaBancaria/aplication/service/PagamentoDomainService.java
@@ -5,9 +5,6 @@ import com.senai.contaBancaria.domain.entity.Taxa;
 import com.senai.contaBancaria.domain.enums.FormaPagamento;
 import com.senai.contaBancaria.domain.exceptions.FormaDePagamentoInvalidaException;
 import com.senai.contaBancaria.domain.exceptions.SaldoInsuficienteException;
-import com.senai.contaBancaria.domain.repository.TaxaRepository;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -15,53 +12,50 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PagamentoDomainService {
-    private final TaxaRepository repository;
+    private final ContaService contaService;
 
     @PreAuthorize("hasRole('CLIENTE')")
-    public FormaPagamento validarFormaPagamento(String formaPagamento) {
+    public FormaPagamento validarFormaPagamento(String formaPagamento){
         boolean formaPagamentoExiste = false;
-        for (FormaPagamento formaPossivel : FormaPagamento.values()) {
+        for(FormaPagamento formaPossivel : FormaPagamento.values()) {
             if (formaPagamento.equals(formaPossivel.name())) {
                 formaPagamentoExiste = true;
                 break;
             }
         }
-        if (!formaPagamentoExiste) throw new FormaDePagamentoInvalidaException(formaPagamento);
+        if(!formaPagamentoExiste) throw new FormaDePagamentoInvalidaException(formaPagamento);
         return FormaPagamento.valueOf(formaPagamento);
     }
 
     @PreAuthorize("hasRole('CLIENTE')")
-    public BigDecimal calcularTaxa(@NotNull(message = "O valor do serviço pago não pode ser nulo.") @Positive(message = "O valor do serviço pago deve ser maior que zero.") BigDecimal formaPagamento, BigDecimal valorServico) {
-        List<Taxa> taxas = repository.findAllByFormaPagamentoAndAtivoTrue(formaPagamento);
-
-        BigDecimal valorDasTaxas = BigDecimal.valueOf(0);
+    public BigDecimal calcularTaxa(BigDecimal valorPago, Set<Taxa> taxas){
+        BigDecimal valorTaxa = BigDecimal.valueOf(0);
         for (Taxa taxa : taxas) {
-            valorDasTaxas = valorDasTaxas.add(taxa
-                    .getPercentual()
-                    .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP)
-                    .multiply(valorServico)
+            valorTaxa = valorTaxa.add(taxa
+                    .getValorFixo().add(taxa
+                            .getPercentual()
+                            .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP)
+                            .multiply(valorPago)
+                    )
             );
         }
-
-        return valorDasTaxas;
+        return valorTaxa;
     }
-}
+
     @PreAuthorize("hasRole('CLIENTE')")
     public BigDecimal validarSaldo(Long numero, BigDecimal valorPago, BigDecimal valorTaxa) {
         Conta conta = contaService.procurarContaAtiva(numero);
 
         BigDecimal valorTotal = valorPago.add(valorTaxa);
 
-
         if(conta.getSaldo().compareTo(valorTotal) < 0) throw new SaldoInsuficienteException("pagamento");
 
         return valorTotal;
     }
-
-
+}
